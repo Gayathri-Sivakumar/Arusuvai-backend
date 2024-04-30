7bab879d9b5a223ed57cca8d05a08d9f3930679e
Gayathri-Sivakumar <sivakumarg.19@uom.lk>
2024-04-30 10:18:29 +0530

    user details API added

--- controllers/userDetails.js
+++ controllers/userDetails.js
@@ -2,7 +2,20 @@ const userModel = require("../models/userModel");
 
 async function userDetailsController(req, res) {
   try {
-    //get user details
+    // Get user details
+    const user = await userModel.findById(req.userId);
+
+    console.log("User details:", user);
+
+    // Respond with user's name and profile picture
+    res.status(200).json({
+      data: {
+        name: user.name,
+        pic: user.profilePic,
+      },
+      error: false,
+      success: true,
+    });
   } catch (error) {
     res.status(500).json({
       message: error.message || error.toString(),
@@ -11,4 +24,5 @@ async function userDetailsController(req, res) {
     });
   }
 }
+
 module.exports = userDetailsController;
--- middleware/authToken.js
+++ middleware/authToken.js
@@ -2,16 +2,20 @@ const jwt = require("jsonwebtoken");
 
 async function authToken(req, res, next) {
   try {
-    const token =
-      req.cookies?.token || req.headers?.authorization?.split(" ")[1];
-    console.log(token);
+    const token = req.cookies.token;
+
     if (!token) {
       return res.status(401).json({ error: "user not loged in" });
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
-    req.userId = decoded.id;
-    next();
+    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(401).json({ error: "Unauthorized token" });
+      }
+      req.userId = decoded?.id;
+      req.userEmail = decoded.email;
+      console.log(req.userEmail);
+      next();
+    });
   } catch (error) {
     res.status(401).json({ error: "Unauthorized" });
   }
